a146da1aeea2eea02694f50460cfb39be4f29150
Maximka <boriskrendebober@gmail.com>
2024-11-23 17:03:06 +0300

    deleted shutku

--- frontend/src/Components/CharacterOverview/CharacterPreview.js
+++ frontend/src/Components/CharacterOverview/CharacterPreview.js
@@ -2,7 +2,7 @@ import CharacterPage from "../../Pages/CharacterPage";
 import styles from "./CharacterPreview.module.css";
 
 export default function CharacterPreview({ character_data ,loadNextPage}) {
-    character_data = character_data || [["Name", "Adolf"], ["Surname", "Hitler"]];
+    character_data = character_data || [["Attr1", "Value1"], ["Attr2", "Value2"]];
     console.log(character_data);
 
     // Convert data into an array of <h4> elements with "key:value" format.
