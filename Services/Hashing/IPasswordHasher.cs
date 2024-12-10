5d85a2e711f72e5ebc2b5a1a3173896e5d50058e
Maximka <boriskrendebober@gmail.com>
2024-12-10 19:36:27 +0300

    Удалил лишний hashing

--- Services/Hashing/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-namespace Hashing;
-
-
-public interface IPasswordHasherService
-{
-    public string HashPassword(string password);
-    public bool CheckPassword(string hashedPassword, string password);
-}
